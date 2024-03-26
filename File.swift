6d034b8cdeb97618034231740ef330d92c849c0c
Daniil Kuznetsov <VKBcorp@yandex.ru>
2024-03-26 10:33:39 +0700

    no message

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  remote test
+//
+//  Created by Даниил Кузнецов on 26.03.2024.
+//
+
+import Foundation
+// test for my repo 
---
+++ remote test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  remote test
+//
+//  Created by Даниил Кузнецов on 26.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
